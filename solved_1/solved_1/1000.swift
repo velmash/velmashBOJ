162f457d8eb35e32fc789d1635dff00d8cb73ab1
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-07-01 18:41:15 +0900

    :wrench: swift 풀이용 TestProject 추가 (section 1)

---
+++ solved_1/solved_1/1000.swift
@@ -0,0 +1,20 @@
+//
+//  1000.swift
+//  solved_1
+//
+//  Created by 윤형찬 on 7/1/24.
+//
+
+import Foundation
+
+class question1000 {
+    static func solved() {
+        let line1 = readLine()
+        let line2 = readLine()
+        let line3 = readLine()
+        let line4 = readLine()
+        let line5 = readLine()
+
+        [line1,line2,line3,line4,line5].map{print($0)}
+    }
+}
---
+++ solved_1/solved_1/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  solved_1
+//
+//  Created by 윤형찬 on 7/1/24.
+//
+
+import Foundation
+
+question1000.solved()
